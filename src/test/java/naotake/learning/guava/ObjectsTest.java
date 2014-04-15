f4bd788fa8f13bc5ef70c4cbc93503377987cf77
naotake.k <naotake.87@gmail.com>
2014-04-15 22:36:17 +0900

    Learning is Objects on UnitTest.

---
+++ src/test/java/naotake/learning/guava/ObjectsTest.java
@@ -0,0 +1,41 @@
+package naotake.learning.guava;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Objects;
+
+import org.junit.Test;
+
+/**
+ * {@link Objects}に対するテストクラス。
+ * 
+ * @author naotake
+ */
+public class ObjectsTest {
+
+    private Object obj = new Object();
+
+    @Test
+    public void testIsNull() {
+        assertThat(Objects.isNull(obj), is(false));
+        assertThat(Objects.isNull(null), is(true));
+    }
+
+    @Test
+    public void testNonNull() {
+        assertThat(Objects.nonNull(obj), is(true));
+        assertThat(Objects.nonNull(null), is(false));
+    }
+
+    @Test
+    public void testRequireNonNull() {
+        assertThat(Objects.requireNonNull(obj), notNullValue());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testRequireNonNullOnNull() {
+        Objects.requireNonNull(null);
+    }
+}
